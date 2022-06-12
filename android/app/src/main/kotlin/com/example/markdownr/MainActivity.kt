4226a2680064b22a2a261b1be081cd090f1f085a
sanzoghenzo <andrea.ghensi@gmail.com>
2022-06-12 22:15:04 +0200

    feat: add title, url option
    - H1 title is always added at the start of the content
    - added a checkable menu item to optionally include the url right below the title

--- android/app/src/main/kotlin/com/example/markdownr/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/markdownr/MainActivity.kt
@@ -6,6 +6,7 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import net.dankito.readability4j.extended.Readability4JExtended
 
+
 class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
@@ -17,21 +18,24 @@ class MainActivity: FlutterActivity() {
             if (call.method == "makeReadable") {
                 val html: String? = call.argument<String>("html")
                 val url: String? = call.argument<String>("url")
-                val readableHtml = makeReadable(html, url)
-                result.success(readableHtml)
+                val (title, readableHtml) = makeReadable(html, url)
+                val arguments = HashMap<String, String>()
+                arguments["title"] = title
+                arguments["html"] = readableHtml
+                result.success(arguments)
             } else {
                 result.notImplemented()
             }
         }
     }
 
-    private fun makeReadable(html: String?, url: String?): String {
+    private fun makeReadable(html: String?, url: String?): Pair<String, String> {
         if (html == null || url == null) {
-            return ""
+            return Pair("", "")
         }
         val readabilityService = Readability4JExtended(url, html)
         val article = readabilityService.parse()
-        if (article.content != null) return article.content!!
-        return ""
+        if (article.content != null) return Pair(article.title!!, article.content!!)
+        return Pair("", "")
     }
 }
