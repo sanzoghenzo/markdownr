9018bc743b7f8b0e642e2c458aaeb7fe60ed98e6
Andrea Ghensi <andrea.ghensi@gmail.com>
2023-11-09 08:16:08 +0100

    feat: real time settings update
    Parsed article is now a class,
    so that parts can be shown/hidden without re-downloading it.
    This paves the way to templates.

--- test/converter_test.dart
+++ test/converter_test.dart
@@ -3,13 +3,11 @@ import 'package:markdownr/converter.dart';
 import 'package:markdownr/httpclient.dart';
 import 'package:markdownr/notifications.dart';
 import 'package:markdownr/readability.dart';
-import 'package:markdownr/settings.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 @GenerateNiceMocks([
   MockSpec<HttpClient>(),
-  MockSpec<SettingsRepository>(),
   MockSpec<NotificationService>(),
   MockSpec<ReadabilityService>(),
 ])
@@ -18,19 +16,16 @@ import 'converter_test.mocks.dart';
 void main() {
   group('Url2MdConverter', () {
     late MockHttpClient mockHttpClient;
-    late MockSettingsRepository mockSettingsRepository;
     late MockNotificationService mockNotificationService;
     late Url2MdConverter url2mdConverter;
     late ReadabilityService mockReadabilityService;
 
     setUp(() {
       mockHttpClient = MockHttpClient();
-      mockSettingsRepository = MockSettingsRepository();
       mockNotificationService = MockNotificationService();
       mockReadabilityService = MockReadabilityService();
       url2mdConverter = Url2MdConverter(
         httpClient: mockHttpClient,
-        settingsRepository: mockSettingsRepository,
         notificationService: mockNotificationService,
         readabilityService: mockReadabilityService,
       );
@@ -41,32 +36,30 @@ void main() {
       const content = '<h2>Test Header</h2><p>Test paragraph</p>';
       const html =
           '<html><head><title>Test Title</title></head><body>$content</body></html>';
-      const expectedMarkdown =
-          '# Test Title\n\n## Test Header\n\nTest paragraph';
       when(mockHttpClient.getPage(url)).thenAnswer((_) async => html);
-      when(mockSettingsRepository.getBool('includeFrontMatter'))
-          .thenAnswer((_) => false);
-      when(mockSettingsRepository.getBool('includeSourceLink'))
-          .thenAnswer((_) => false);
-      when(mockSettingsRepository.getBool('includeExcerpt'))
-          .thenAnswer((_) => false);
-      when(mockSettingsRepository.getBool('includeBody', defaultValue: true))
-          .thenAnswer((_) => true);
       when(mockReadabilityService.makeReadable(html, url)).thenAnswer(
           (_) async => const ReadabilityOutput(
               content: '<div>$content</div>',
               title: "Test Title",
-              author: "",
-              excerpt: ""));
-      final markdown = await url2mdConverter.convert(url: url);
-      expect(markdown, expectedMarkdown);
+              author: "Myself",
+              excerpt: "A really good article"));
+      final markdown = await url2mdConverter.convertPage(url: url);
+      expect(markdown.url, url);
+      expect(markdown.content, "## Test Header\n\nTest paragraph");
+      expect(markdown.title, "Test Title");
+      expect(markdown.author, "Myself");
+      expect(markdown.excerpt, "A really good article");
     });
 
     test('convert should handle exceptions', () async {
       const url = 'https://example.com';
       when(mockHttpClient.getPage(url)).thenThrow(Exception());
-      final markdown = await url2mdConverter.convert(url: url);
-      expect(markdown, '');
+      final markdown = await url2mdConverter.convertPage(url: url);
+      expect(markdown.url, url);
+      expect(markdown.content, "");
+      expect(markdown.title, "");
+      expect(markdown.author, "");
+      expect(markdown.excerpt, "");
     });
   });
 }
