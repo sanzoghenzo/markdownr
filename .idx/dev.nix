ca01a08c3172d343674aa7db902f7c04dab1a104
andrea.ghensi <andrea.ghensi@gmail.com>
2024-06-02 21:02:41 +0000

    build: add idx dev config

---
+++ .idx/dev.nix
@@ -0,0 +1,42 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  idx.extensions = [
+    "Dart-Code.dart-code"
+    "Dart-Code.flutter"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "web-server"
+          "--web-hostname"
+          "0.0.0.0"
+          "--web-port"
+          "$PORT"
+        ];
+        manager = "flutter";
+      };
+      android = {
+        command = [
+          "flutter"
+          "run"
+          "--machine"
+          "-d"
+          "android"
+          "-d"
+          "emulator-5554"
+        ];
+        manager = "flutter";
+      };
+    };
+  };
+}
